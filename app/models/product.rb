aa28bcd7d6908bbc131f5af56644a3726cb9e4c6
secretpray <a.p.ruban@gmail.com>
2022-03-07 08:05:00 +0200

    Product validaions, seed.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+
+  MIN_LENGTH             = 2
+  MAX_NAME_LENGTH        = 100
+  MAX_DESCRIPTION_LENGTH = 1000
+
+  validates_presence_of :name, :description
+  validates :name, length: { in: MIN_LENGTH..MAX_NAME_LENGTH }
+  validates :description, length: { in: MIN_LENGTH..MAX_DESCRIPTION_LENGTH }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+return unless Rails.env == "development"
+
+system 'clear'
+
+puts 'Destroy all records'
+puts '*' * 80
+
+Product.destroy_all
+
+puts 'Create new records'
+puts '*' * 80
+
+MAX_PODUCTS = 21
+
+print '.'
+
+MAX_PODUCTS.times do
+  name = Faker::Name.unique.name
+  description = Faker::Company.catch_phrase
+  Product.create!(name: name, description: description)
+  print '.'
+end
+
+puts ' '
+puts ' '
+puts "That's all folks!"
+puts '*' * 80
+p "Created #{Product.count} #{'product'.pluralize(Product.count)}"
